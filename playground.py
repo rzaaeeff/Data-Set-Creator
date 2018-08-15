6fa75a3b9b03bf8f39b147f3f51213ca6c6dc53d
Heydar Rzayev <rzaaeeff@gmail.com>
2018-08-15 15:28:29 +0400

    feat: [Playground] Sort letter counts

--- playground.py
+++ playground.py
@@ -2,5 +2,9 @@ import db
 
 conn = db.create_connection(db.DATABASE)
 counts = db.show_counts_of_all_letters(conn)
+sorted_counts = {}
 
-print(counts)
+for key in sorted(counts.keys(), reverse=True):
+    sorted_counts[key] = counts[key]
+
+print(sorted_counts)
