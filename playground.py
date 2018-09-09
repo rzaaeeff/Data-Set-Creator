27716a665053c2651e74d6f2e7f4ec97fc1d8cb2
Heydar Rzayev <rzaaeeff@gmail.com>
2018-09-09 21:42:08 +0400

    feat: [Playground] Ignore 0s

--- playground.py
+++ playground.py
@@ -5,6 +5,7 @@ counts = db.show_counts_of_all_letters(conn)
 sorted_counts = {}
 
 for key in sorted(counts.keys(), reverse=True):
-    sorted_counts[key] = counts[key]
+    if (counts[key] is not 0):
+        sorted_counts[key] = counts[key]
 
 print(sorted_counts)
