5038a9edae678fea3640c5de1b8f47aa86db4d72
Heydar Rzayev <rzaaeeff@gmail.com>
2018-10-14 21:28:46 +0400

    refactor: [Playground] Remove sorting
    For some reason sorting didn't work, so I removed it.

--- playground.py
+++ playground.py
@@ -2,10 +2,11 @@ import db
 
 conn = db.create_connection(db.DATABASE)
 counts = db.show_counts_of_all_letters(conn)
-sorted_counts = {}
-
-for key in sorted(counts.keys(), reverse=True):
-    if (counts[key] is not 0):
-        sorted_counts[key] = counts[key]
-
-print(sorted_counts)
+#sorted_counts = {}
+#
+#for key in sorted(counts.keys(), reverse=True):
+#    if (counts[key] is not 0):
+#        sorted_counts[key] = counts[key]
+#
+#print(sorted_counts)
+print(counts)
